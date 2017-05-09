478e27684cd609d62039bb65ac162a82442f5e70
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-05-09 14:54:44 -0700

    kicking off API defn

--- landmapper/views.py
+++ landmapper/views.py
@@ -13,3 +13,44 @@ def index(request):
         }
     }
     return HttpResponse(template.render(context, request))
+
+def about(request):
+    template = loader.get_template('generic.html')
+    context = {
+        'title': 'LandMapper',
+        'content': {
+            'title': 'Content Title',
+            'copy': '<p>Stuff <b>about</b> stuff!</p>',
+        }
+    }
+    return HttpResponse(template.render(context, request))
+
+def help(request):
+    template = loader.get_template('generic.html')
+    context = {
+        'title': 'LandMapper',
+        'content': {
+            'title': 'Help Title',
+            'copy': '<p>Stuff <b>help</b> stuff!</p>',
+        }
+    }
+    return HttpResponse(template.render(context, request))
+
+def portfolio(request, portfolio_id):
+    #TODO write this template
+    template = loader.get_template('portfolio.html')
+    user = request.GET.user
+    #TODO Check this syntax
+    #if user is authenticated
+    #TODO Write this model
+    from landmapper.models import Portfolio
+    user_portfolio = Portfolio.objects.get(user=user)
+    context = {
+        'title': 'LandMapper',
+        'content': {
+            'title': 'Your Portfolio',
+            'copy': '<p>blah blah</p>',
+        },
+        'portfolio': user_portfolio.as_dict()
+    }
+    return HttpResponse(template.render(context, request))
