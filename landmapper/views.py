0d0e6e559c7138107c47b4b66df9c6fc653df229
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-06-09 17:51:45 -0700

    fixing reference to settings file

--- landmapper/views.py
+++ landmapper/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
-from django.conf import settings
+from marineplanner import settings
 # TODO tax lot model
 # from .models import TaxLots
 
