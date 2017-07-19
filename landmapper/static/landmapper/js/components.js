50de468fa1f5d9c2cb1636128c8f139770b3d69b
David Pollard <pollardld@gmail.com>
2017-07-19 15:29:06 -0700

    add help button

--- landmapper/static/landmapper/js/components.js
+++ landmapper/static/landmapper/js/components.js
@@ -1,25 +1,24 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
-
-  window.setTimeout(function() {
+  $('#help').on('click', function() {
     $('.t1').tapTarget('open');
     window.setTimeout(function() {
       Materialize.showStaggeredList('#start-steps');
     }, 1000);
-  }, 500);
 
-  window.setTimeout(function() {
-    $('.t1').tapTarget('close');
-  }, 5000);
-
-  window.setTimeout(function() {
-    $('.t2').tapTarget('open');
     window.setTimeout(function() {
-      Materialize.showStaggeredList('#start-step3');
-    }, 1000);
-  }, 5500);
+      $('.t1').tapTarget('close');
+    }, 5000);
 
-  window.setTimeout(function() {
-    $('.t2').tapTarget('close');
-  }, 10000);
+    window.setTimeout(function() {
+      $('.t2').tapTarget('open');
+      window.setTimeout(function() {
+        Materialize.showStaggeredList('#start-step3');
+      }, 1000);
+    }, 5500);
+
+    window.setTimeout(function() {
+      $('.t2').tapTarget('close');
+    }, 10000);
+  });
 });
