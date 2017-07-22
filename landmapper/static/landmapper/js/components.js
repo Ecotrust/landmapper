6f2225ab78d72d003981af3387a314ead8d95e54
David Pollard <pollardld@gmail.com>
2017-07-21 18:11:21 -0700

    sticky footer and generic styles

--- landmapper/static/landmapper/js/components.js
+++ landmapper/static/landmapper/js/components.js
@@ -6,20 +6,8 @@ $(document).ready(function() {
     window.setTimeout(function() {
       Materialize.showStaggeredList('#start-steps');
     }, 1000);
-
     window.setTimeout(function() {
       $('.t1').tapTarget('close');
-    }, 5000);
-
-    window.setTimeout(function() {
-      $('.t2').tapTarget('open');
-      window.setTimeout(function() {
-        Materialize.showStaggeredList('#start-step3');
-      }, 1000);
-    }, 5500);
-
-    window.setTimeout(function() {
-      $('.t2').tapTarget('close');
-    }, 10000);
+    }, 9000);
   });
 });
