92498894ae73d71270ff4fe8b80469f762487c56
Ryan Hodges <ryan.d.hodges@gmail.com>
2018-11-25 13:22:07 -0800

    adding provisioning to get requirements installed for landmapper

---
+++ scripts/vagrant_provision.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PROJECT_NAME=$1
+MODULE_NAME=landmapper
+
+PROJECT_DIR=/usr/local/apps/$PROJECT_NAME
+VIRTUALENV_DIR=$PROJECT_DIR/env
+MODULE_DIR=$PROJECT_DIR/apps/$MODULE_NAME
+
+PYTHON=$VIRTUALENV_DIR/bin/python
+PIP=$VIRTUALENV_DIR/bin/pip
+
+$PIP install -r $MODULE_DIR/requirements.txt
