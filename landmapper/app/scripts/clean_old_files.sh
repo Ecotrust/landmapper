831c0fc83999bc84510ee9bbd1a6112b7c2c349d
Ryan Hodges <ryan.d.hodges@gmail.com>
2024-06-11 09:58:42 -0700

    clean old files now clears out old fixtures created by cron jobs

--- landmapper/app/scripts/clean_old_files.sh
+++ landmapper/app/scripts/clean_old_files.sh
@@ -6,15 +6,17 @@ EPHEMERAL_DIRECTORIES=("/usr/local/apps/forestplanner/lot/landmapper/static/land
 DAYS_OLD=7
 
 # for each folder containing volatile/ephemeral data...
-for folder in ${EPHEMERAL_DIRECTORIES[@]}; do
-  # get every file in the folder older than $DAYS_OLD days
-  readarray -d '' ephemeral_files < <(/usr/bin/find $folder -name "*.*" -mtime +$DAYS_OLD -print0)
-  # for every file in the folder...
-  for ephemeral_file in "${ephemeral_files[@]}"; do
-      # delete old file
-      /bin/rm "${ephemeral_file[@]}"
-  done
+# for folder in ${EPHEMERAL_DIRECTORIES[@]}; do
+#   # get every file in the folder older than $DAYS_OLD days
+#   readarray -d '' ephemeral_files < <(/usr/bin/find $folder -name "*.*" -mtime +$DAYS_OLD -print0)
+#   # for every file in the folder...
+#   for ephemeral_file in "${ephemeral_files[@]}"; do
+#       # delete old file
+#       /bin/rm "${ephemeral_file[@]}"
+#   done
 
-done
+# done
 
 /usr/bin/find /usr/local/apps/pdf/ -type f ! -newerat $(/usr/bin/date -d "-1 month" +%Y-%m-%d) | /usr/bin/xargs -d '\n' /usr/bin/rm -rf
+/usr/bin/find /usr/local/apps/landmapper/backups/ -type f ! -newerat $(/usr/bin/date -d "-10 day" +%Y-%m-%d) | grep "_userpropertyfixture.json" | /usr/bin/xargs -d '\n' /usr/bin/rm -rf
+/usr/bin/find /usr/local/apps/landmapper/backups/ -type f ! -newerat $(/usr/bin/date -d "-10 day" +%Y-%m-%d) | grep "_userprofilefixture.json" | /usr/bin/xargs -d '\n' /usr/bin/rm -rf
